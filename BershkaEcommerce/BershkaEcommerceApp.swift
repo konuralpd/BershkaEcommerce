235205cf1efbab4c9036ec88467275dee95cd1ed
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-07-17 03:58:53 +0300

    Initial Commit

---
+++ BershkaEcommerce/BershkaEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BershkaEcommerceApp.swift
+//  BershkaEcommerce
+//
+//  Created by Mac on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BershkaEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
